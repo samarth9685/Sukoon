d421401fcba39d3fbd1a767ccaab8f6c64f5214f
Samarth Agarwal <144302485+samarth9685@users.noreply.github.com>
2025-07-25 23:18:01 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5004/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   // In production, VITE_API_BASE_URL comes from `.env`
 });
 
