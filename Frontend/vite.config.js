21b573b1a179ddd4ad26c56482b3ba1668b9d1e1
Samarth Agarwal <144302485+samarth9685@users.noreply.github.com>
2025-07-27 09:07:18 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   build: {
-    outDir: 'dist',
-  },
-  // Needed for SPA routing on Render
-  server: {
-    historyApiFallback: true,
+    outDir: 'dist'
   }
 })
